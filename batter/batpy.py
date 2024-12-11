42f44886b0d0633fd465671777411ad950cb0c5d
Yuxuan Zhuang <yuxuan.zhuang@stanford.edu>
2024-12-11 01:08:53 -0500

    sep sim to different groups

--- batter/batpy.py
+++ batter/batpy.py
@@ -1295,6 +1295,7 @@ def generate_frontier_files(version=24):
             'eqnpt.in_00',
             'eqnpt.in_01', 'eqnpt.in_02',
             'eqnpt.in_03', 'eqnpt.in_04',
+            'mdin.in', 'mdin.in.extend'
         ],
         'sdr': [
             'mini.in',
@@ -1303,6 +1304,7 @@ def generate_frontier_files(version=24):
             'eqnpt.in_00',
             'eqnpt.in_01', 'eqnpt.in_02',
             'eqnpt.in_03', 'eqnpt.in_04',
+            'mdin.in', 'mdin.in.extend'
         ],
     }
     # write a groupfile for each component
@@ -1311,7 +1313,7 @@ def generate_frontier_files(version=24):
         """
         Write a groupfile for each component in the pose
         """
-        all_replicates = []
+        all_replicates = {comp: [] for comp in components}
 
         pose_name = f'fe/{pose}/'
         os.makedirs(pose_name, exist_ok=True)
@@ -1336,6 +1338,8 @@ def generate_frontier_files(version=24):
                         inpcrd = f'{sim_folder_name}/full.inpcrd'
                         mdinput = f'{sim_folder_name}/{stage.split("_")[0]}'
                         # Read and modify the MD input file to update the relative path
+                        if stage in ['mdin.in', 'mdin.in.extend']:
+                            mdinput = mdinput.replace(stage, 'mdin-02')
                         with open(f'fe/{mdinput}', 'r') as infile:
                             input_lines = infile.readlines()
 
@@ -1348,18 +1352,41 @@ def generate_frontier_files(version=24):
                                     line = f"output_file = '{sim_folder_name}/cmass.txt'\n"
                                 if 'disang' in line:
                                     line = f"DISANG={sim_folder_name}/disang.rest\n"
+                                if stage == 'mdin.in' or stage == 'mdin.in.extend':
+                                    if 'nstlim' in line:
+                                        inpcrd_file = f'fe/{sim_folder_name}/full.inpcrd'
+                                        # read the second line of the inpcrd file
+                                        with open(inpcrd_file, 'r') as infile:
+                                            lines = infile.readlines()
+                                            n_atoms = int(lines[1])
+                                        performance = calculate_performance(n_atoms, component)
+                                        n_steps = int(20 / 60 / 24 * performance * 1000 * 1000 / 4)
+                                        n_steps = int(n_steps // 100000 * 100000)
+                                        line = f'  nstlim = {n_steps},\n'
                                 outfile.write(line)
 
                         f.write(f'# {component} {i} {stage}\n')
-                        f.write(
-                            f'-O -i {sim_folder_name}/{stage.split("_")[0]}_frontier -p {prmtop} -c {stage_previous.replace("REPXXX", f"{i:02d}")} '
-                            f'-o {sim_folder_name}/{stage}.out -r {sim_folder_name}/{stage}.rst7 -x {sim_folder_name}/{stage}.nc '
-                            f'-ref {inpcrd} -inf {sim_folder_name}/{stage}.mdinfo -l {sim_folder_name}/{stage}.log '
-                            f'-e {sim_folder_name}/{stage}.mden\n'
-                        )
-                        if stage == 'eqnpt.in_04':
-                            all_replicates.append(sim_folder_name)
+                        if stage == 'mdin.in':
+                            f.write(f'-O -i {sim_folder_name}/mdin_frontier -p {sim_folder_name}/full.hmr.prmtop -c {sim_folder_name}/eqnpt.in_04.rst7 '
+                                    f'-o {sim_folder_name}/mdin-00.out -r {sim_folder_name}/mdin-00.rst7 -x {sim_folder_name}/mdin-00.nc '
+                                    f'-ref {sim_folder_name}/full.inpcrd -inf {sim_folder_name}/mdinfo -l {sim_folder_name}/mdin-00.log '
+                                    f'-e {sim_folder_name}/mdin-00.mden\n')
+                        elif stage == 'mdin.in.extend':
+                            f.write(f'-O -i {sim_folder_name}/mdin_frontier -p {sim_folder_name}/full.hmr.prmtop -c {sim_folder_name}/mdin-CURRNUM.rst7 '
+                                    f'-o {sim_folder_name}/mdin-NEXTNUM.out -r {sim_folder_name}/mdin-NEXTNUM.rst7 -x {sim_folder_name}/mdin-NEXTNUM.nc '
+                                    f'-ref {sim_folder_name}/full.inpcrd -inf {sim_folder_name}/mdinfo -l {sim_folder_name}/mdin-NEXTNUM.log '
+                                    f'-e {sim_folder_name}/mdin-NEXTNUM.mden\n')
+                        else:
+                            f.write(
+                                f'-O -i {sim_folder_name}/{stage.split("_")[0]}_frontier -p {prmtop} -c {stage_previous.replace("REPXXX", f"{i:02d}")} '
+                                f'-o {sim_folder_name}/{stage}.out -r {sim_folder_name}/{stage}.rst7 -x {sim_folder_name}/{stage}.nc '
+                                f'-ref {inpcrd} -inf {sim_folder_name}/{stage}.mdinfo -l {sim_folder_name}/{stage}.log '
+                                f'-e {sim_folder_name}/{stage}.mden\n'
+                            )
+                        if stage == 'mdin.in':
+                            all_replicates[component].append(f'{sim_folder_name}')
                     stage_previous = f'{sim_folder_temp}REPXXX/{stage}.rst7'
+        logger.debug(f'all_replicates: {all_replicates}')
         return all_replicates
 
     def write_sbatch_file(pose, components):
@@ -1378,6 +1405,8 @@ def generate_frontier_files(version=24):
             ]
             logger.debug(f'groupfile_names: {groupfile_names}')
             for g_name in groupfile_names:
+                if 'mdin.in' in g_name:
+                    continue
                 if component in ['x', 'e', 'v', 'w', 'f']:
                     n_sims = len(lambdas)
                 else:
@@ -1397,6 +1426,8 @@ def generate_frontier_files(version=24):
                      .replace('FEP_SIM_XXX', f'{folder}_{component}_{pose}') for line in lines]
             with open(sbatch_file, 'w') as f:
                 f.writelines(lines)
+             
+
     def calculate_performance(n_atoms, comp):
         # Very rough estimate of the performance of the simulations
         # for 200000-atom systems: rest: 100 ns/day, sdr: 50 ns/day
@@ -1413,117 +1444,95 @@ def generate_frontier_files(version=24):
             else:
                 return 40
 
-        
-    def write_groupfile_production(all_replicates):
-        # Read and modify the MD input file to update the relative path
+    def write_production_sbatch(all_replicates):
+        sbatch_file = f'fe/fep_md.sbatch'
+        sbatch_extend_file = f'fe/fep_md_extend.sbatch'
 
-        for replicate in all_replicates:
-            # get a estimated number of steps can be finished in 30 mins
-            # that is the current tested submission time
-            inpcrd_file = f'fe/{replicate}/full.inpcrd'
-            # read the second line of the inpcrd file
-            with open(inpcrd_file, 'r') as infile:
-                lines = infile.readlines()
-                n_atoms = int(lines[1])
-                comp = replicate.split('/')[-1][0]
-            performance = calculate_performance(n_atoms, comp)
-            # estimate the number of steps to run in 20 mins
-            n_steps = int(20 / 60 / 24 * performance * 1000 * 1000 / 4)
-            n_steps = int(n_steps // 100000 * 100000)
-
-            with open(f'fe/{replicate}/mdin-02', 'r') as infile:
-                input_lines = infile.readlines()
-            new_mdinput = f'fe/{replicate}/mdin_frontier'
-            with open(new_mdinput, 'w') as outfile:
-                for line in input_lines:
-                    if 'cv_file' in line:
-                        line = f"cv_file = '{replicate}/cv.in'\n"
-                    if 'output_file' in line:
-                        line = f"output_file = '{replicate}/cmass.txt'\n"
-                    if 'disang' in line:
-                        line = f"DISANG={replicate}/disang.rest\n"
-                    if 'nstlim' in line:
-                        line = f'  nstlim = {n_steps},\n'
-                    outfile.write(line)
-
-    def write_production(all_replicates):
         file_temp = f'{frontier_files}/fep_run.sbatch'
         temp_lines = open(file_temp).readlines()
         temp_lines.append(f'\n\n\n')
-
-
-        # write the sbatch file for one long production
-        groupfile_name_prod = 'mdin.groupfile'
-        groupfile_name_prod_extend = 'mdin_extend.groupfile'
-
-        n_sims = len(all_replicates)
-        n_nodes = int(np.ceil(n_sims / 8))
-
-        # run the production
-        sbatch_file = f'fe/fep_md.sbatch'
+                   # run the production
         folder = os.getcwd()
-        folder = '_'.join(folder.split(os.sep)[-4:])
-        lines = [
+        folder = '_'.join(folder.split(os.sep)[-4:]) 
+        n_sims = 0
+        for replicates_pose in all_replicates:
+            for comp, rep in replicates_pose.items():
+                n_sims += len(rep)
+        n_nodes = int(np.ceil(n_sims / 8))
+        temp_lines = [
             line.replace('NUM_NODES', str(n_nodes))
                 .replace('FEP_SIM_XXX', f'fep_md_{folder}')
             for line in temp_lines]
-        lines.append(
-            f'srun -N {n_nodes} -n {n_sims} pmemd.hip_SPFP.MPI -ng {n_sims} -groupfile {groupfile_name_prod}\n'
-        )
+
         with open(sbatch_file, 'w') as f:
-            f.writelines(lines)
+            f.writelines(temp_lines)
 
-        # extend the production
-        sbatch_file = f'fe/fep_md_extend.sbatch'
-        lines = [
-            line.replace('NUM_NODES', str(n_nodes))
-                .replace('FEP_SIM_XXX', f'fep_md_{folder}')
-            for line in temp_lines
-        ]
-        lines.extend([
-            '# Get the latest mdin-xx.rst7 file in the current directory\n',
-            'latest_file=$(ls pose0/sdr/e00/mdin-??.rst7 2>/dev/null | sort | tail -n 1)\n\n',
-            '# Check if any mdin-xx.rst7 files exist\n',
-            'if [[ -z "$latest_file" ]]; then\n',
-            '  echo "No old production files found in the current directory."\n',
-            '  echo "Run sbatch fep_md.sbatch."\n',
-            '  exit 1\n',
-            'fi\n\n',
-            '# Extract the latest number (xx) and calculate the next number\n',
-            'latest_num=$(echo "$latest_file" | grep -oP \'(?<=-)\d{2}(?=\\.rst7)\')\n',
-            'next_num=$(printf "%02d" $((10#$latest_num + 1)))\n\n',
-            '# Replace REPNUM in the groupfile with the current number\n',
-            'sed "s/CURRNUM/$latest_num/g" mdin_extend.groupfile > temp_mdin.groupfile\n',
-            '# Replace NEXTNUM in the groupfile with the next number\n',
-            'sed "s/NEXTNUM/$next_num/g" temp_mdin.groupfile > current_mdin.groupfile\n\n',
-            '# Run the production simulation\n',
-            f'srun -N {n_nodes} -n {n_sims} pmemd.hip_SPFP.MPI -ng {n_sims} -groupfile current_mdin.groupfile\n'
-        ])
-        with open(sbatch_file, 'w') as f:
-            f.writelines(lines)
+        with open(sbatch_extend_file, 'w') as f:
+            f.writelines(temp_lines)
+            f.writelines(
+            [
+                '# Get the latest mdin-xx.rst7 file in the current directory\n',
+                'latest_file=$(ls pose0/sdr/e00/mdin-??.rst7 2>/dev/null | sort | tail -n 1)\n\n',
+                '# Check if any mdin-xx.rst7 files exist\n',
+                'if [[ -z "$latest_file" ]]; then\n',
+                '  echo "No old production files found in the current directory."\n',
+                '  echo "Run sbatch fep_md.sbatch."\n',
+                '  exit 1\n',
+                'fi\n\n',
+                '# Extract the latest number (xx) and calculate the next number\n',
+                'latest_num=$(echo "$latest_file" | grep -oP \'(?<=-)\d{2}(?=\\.rst7)\')\n',
+                'next_num=$(printf "%02d" $((10#$latest_num + 1)))\n\n',
+                '# Replace REPNUM in the groupfile with the current number\n',
+                '# sed "s/CURRNUM/$latest_num/g" mdin_extend.groupfile > temp_mdin.groupfile\n',
+                '# Replace NEXTNUM in the groupfile with the next number\n',
+                '# sed "s/NEXTNUM/$next_num/g" temp_mdin.groupfile > current_mdin.groupfile\n\n',
+                '# Run the production simulation\n',
+            ]
+            )
 
-        with open(f'fe/{groupfile_name_prod}', 'w') as f:
-            for replicate in all_replicates:
-                f.write(f'-O -i {replicate}/mdin_frontier -p {replicate}/full.hmr.prmtop -c {replicate}/eqnpt.in_04.rst7 '
-                        f'-o {replicate}/mdin-00.out -r {replicate}/mdin-00.rst7 -x {replicate}/mdin-00.nc '
-                        f'-ref {replicate}/full.inpcrd -inf {replicate}/mdin-00.mdinfo -l {replicate}/mdin-00.log '
-                        f'-e {replicate}/mdin-00.mden\n')
+        for replicates_pose in all_replicates:
+            for comp, rep in replicates_pose.items():
+                pose = rep[0].split('/')[0]
+                groupfile_name_prod = f'{pose}/groupfiles/{comp}_mdin.in.groupfile'
+                groupfile_name_prod_extend = f'{pose}/groupfiles/{comp}_mdin.in.extend.groupfile'
 
-        with open(f'fe/{groupfile_name_prod_extend}', 'w') as f:
-            for replicate in all_replicates:
-                f.write(f'-O -i {replicate}/mdin_frontier -p {replicate}/full.hmr.prmtop -c {replicate}/mdin-CURRNUM.rst7 '
-                        f'-o {replicate}/mdin-NEXTNUM.out -r {replicate}/mdin-NEXTNUM.rst7 -x {replicate}/mdin-NEXTNUM.nc '
-                        f'-ref {replicate}/full.inpcrd -inf {replicate}/mdin-NEXTNUM.mdinfo -l {replicate}/mdin-NEXTNUM.log '
-                        f'-e {replicate}/mdin-NEXTNUM.mden\n')
+                n_nodes = int(np.ceil(len(rep) / 8))
+                n_sims = len(rep)
+                with open(sbatch_file, 'a') as f:
+                    f.writelines(
+                        [
+                    f'# {pose} {comp}\n',
+                    f'srun -N {n_nodes} -n {n_sims} pmemd.hip_SPFP.MPI -ng {n_sims} -groupfile {groupfile_name_prod} &\n',
+                    f'sleep 0.5\n'
+                        ]
+                    )
+                with open(sbatch_extend_file, 'a') as f:
+                    f.writelines(
+                        [ 
+                        f'sed "s/CURRNUM/$latest_num/g" {pose}/groupfiles/{comp}_mdin.in.extend.groupfile > {pose}/groupfiles/{comp}_temp_mdin.groupfile\n',
+                        f'sed "s/NEXTNUM/$next_num/g" {pose}/groupfiles/{comp}_temp_mdin.groupfile > {pose}/groupfiles/{comp}_current_mdin.groupfile\n',
+                        f'# {pose} {comp}\n',
+                        f'srun -N {n_nodes} -n {n_sims} pmemd.hip_SPFP.MPI -ng {n_sims} -groupfile {pose}/groupfiles/{comp}_current_mdin.groupfile &\n',
+                        f'sleep 0.5\n\n'
+                        ]
+                    )
+        # append wait
+        with open(sbatch_file, 'a') as f:
+            f.write('wait\n')
+
+        with open(sbatch_extend_file, 'a') as f:
+            f.write('wait\n')
 
     all_replicates = []
+
     for pose in poses_def:
-        all_replicates.extend(write_2_pose(pose, components))
+        all_replicates.append(write_2_pose(pose, components))
         write_sbatch_file(pose, components)
-        logger.info(f'Generated groupfiles for {pose}')
-    write_production(all_replicates)
-    write_groupfile_production(all_replicates)
+        logger.debug(f'Generated groupfiles for {pose}')
+    logger.debug(all_replicates)
+    write_production_sbatch(all_replicates)
     # copy env.amber.24
     env_amber_file = f'{frontier_files}/env.amber.{version}'
     shutil.copy(env_amber_file, 'fe/env.amber')
     logger.info('Generated groupfiles for all poses')
+
